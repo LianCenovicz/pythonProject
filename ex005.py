798b757eaf7cdc31cad3f37b06444392a1dd2654
LianCenovicz <133685067+LianCenovicz@users.noreply.github.com>
2023-06-22 13:34:54 -0300

    Gg
    Lian

---
+++ ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número:'))
+print('Seu número é {} e seu antecessor é {} e seu sucessor é {}!'.format(n, n-1, n+1))
+
+
---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = int(input('Aluno nota 1 Trimestre:'))
+n2 = int(input('Aluno nota 2 Trimestre:'))
+n3 = int(input('Aluno nota 3 Trimestre:'))
+print('A sua média ficou {}.' .format((n1 + n2 + n3)/2))
+
